99c6d66473cee9b819b90500f3d6c0dfb074a019
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-05 16:05:29 +0100

    Made function to sum salary object employees

--- script.js
+++ script.js
@@ -6,13 +6,39 @@
 // delete user.name;
 
 //function to check if object is empty
-let schedule = {};
-function isEmpty(obj){
-    for(let key in obj){
-        return false;
-    }
-    return true;
-}
-alert(isEmpty(schedule));
-schedule["8:30"] = "get up";
-alert(isEmpty(schedule));
+// let schedule = {};
+// function isEmpty(obj){
+//     for(let key in obj){
+//         return false;
+//     }
+//     return true;
+// }
+// alert(isEmpty(schedule));
+// schedule["8:30"] = "get up";
+// alert(isEmpty(schedule));
+
+//function to sum salaries of employees
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// };
+// function sumSalaries(obj){
+//     let sum = 0;
+//     if(!isEmpty(salaries)){
+//         for(let key in obj){
+//             sum += obj[key];
+//         }
+//         return sum;
+//     }
+//     else{
+//         return 0;
+//     }
+// }
+// function isEmpty(obj){
+//     for(let key in obj){
+//         return false;
+//     }
+//     return true;
+// }
+// alert(sumSalaries(salaries));
