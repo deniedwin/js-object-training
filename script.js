09c6352b2d05dd35e86ef63f73ff4a2ceb2ad75f
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-06 10:32:13 +0100

    Used .map .filter .reduce() methods for arrays
    Made func sumOfTripledEvens using array methods

--- script.js
+++ script.js
@@ -44,19 +44,29 @@
 // alert(sumSalaries(salaries));
 
 //function to multiply only the integers of the object by 2
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
-};
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
 
-function multiplyNumeric(obj){
-    for(let key in obj){
-        if(typeof(obj[key]) === typeof(0)){
-            obj[key] *= 2;
-        }
-    }
-}
-console.log(menu);
-multiplyNumeric(menu);
-console.log(menu);
+// function multiplyNumeric(obj){
+//     for(let key in obj){
+//         if(typeof(obj[key]) === typeof(0)){
+//             obj[key] *= 2;
+//         }
+//     }
+// }
+// console.log(menu);
+// multiplyNumeric(menu);
+// console.log(menu);
+
+//function to use object map, filter and reduce
+const arr = [1,2,3,4,5];
+console.log(arr);
+const arrEven = arr.filter((num) => {return num%2 === 0}); //[2,4]
+console.log(arrEven);
+const arrtriple = arrEven.map((num) => {return num *= 3}); //[6,12]
+console.log(arrtriple);
+const arrSum = arrtriple.reduce((total, currentItem) => {return total += currentItem}); //18
+console.log(arrSum);
