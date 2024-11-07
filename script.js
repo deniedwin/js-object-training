d22a60e32c997152f022e332a2e3cd3a61874c9a
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-07 10:57:08 +0100

    Made function to map two properties of array
    Into another array with the same size

--- script.js
+++ script.js
@@ -118,11 +118,23 @@
 // console.log(copyArrSort);
 // console.log(arr);
 
-//function to extract the names of users from an array of users
-let john = {name: "John", age: 25};
-let pete = {name: "Pete", age: 30};
-let mary = {name: "Mary", age: 28};
-let users = [john, pete, mary];
+// //function to extract the names of users from an array of users
+// let john = {name: "John", age: 25};
+// let pete = {name: "Pete", age: 30};
+// let mary = {name: "Mary", age: 28};
+// let users = [john, pete, mary];
 
-let names = users.map(user => user.name);
-console.log(names);
+// let names = users.map(user => user.name);
+// console.log(names);
+
+// //function to extract id name and surname, merge name and surname as fullName
+// let john = {name: "John", surname: "Smith", id: 1};
+// let pete = {name: "Pete", surname: "Hunt", id: 2};
+// let mary = {name: "Mary", surname: "Key", id: 3};
+
+// let users = [john, pete, mary];
+// let usersMapped = users.map(user => ({fullName: user.name + " " + user.surname, id: user.id}));
+
+// console.table(users);
+// console.table(usersMapped);
+
